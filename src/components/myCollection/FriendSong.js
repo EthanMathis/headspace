edca9aaf5bda63a84c2647a1eec0950b74433f1b
EthanMathis <ethan.mathis@outlook.com>
2021-04-28 14:45:39 -0500

    edit priv is now actually granted when the request has been approved

--- src/components/myCollection/FriendSong.js
+++ src/components/myCollection/FriendSong.js
@@ -1,16 +1,37 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import { getUserSongsBySongId } from "../../modules/userSongManager"
 
 
 
 export const FriendSongCard = ({song}) => {
+    const loggedInUser = JSON.parse(sessionStorage.getItem("headspace_user"))
+
+    const [canEdit, setCanEdit] = useState({})
+
+    const canYouEdit = () => {
+        return getUserSongsBySongId(song.id)
+        .then(response => {
+            setCanEdit(response.find(request => request.userId === loggedInUser))
+        })
+    } 
+    
+    useEffect(() => {
+        canYouEdit()
+    }, [])
+
     return (
         <article className="friendSongCard">
             <h3>{song.title}</h3>
             <h4>Written By: {song.user.name}</h4>
             <Link to={`/friendSong/${song.id}`}>
-                <button type="button" className="btn btn-outline-primary">View</button>
+                <button type="button" className="btn btn-outline-warning">View</button>
             </Link>
+            {canEdit?.canEdit &&  
+            <Link to={`/friendSong/${song.id}/edit`}>
+            <button type="button" className="btn btn-outline-primary">Edit</button>
+            </Link>}
+            
         </article>
     )
 }
