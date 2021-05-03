f4f016ecdbb659d737f66b3a6ae23c78de34ebbd
EthanMathis <ethan.mathis@outlook.com>
2021-05-03 15:41:32 -0500

    moved some functions around in the FriendList. I think it's actually fixed this time

--- src/components/friends/FriendsList.js
+++ src/components/friends/FriendsList.js
@@ -20,25 +20,52 @@ export const FriendsList = () => {
     // sets the state of "friends" to the result of the fetch call so we can loop over it later and create the list 
     const getLoggedInUserFriends = () => {
         return getUserFriends(loggedInUser)
-        .then(friendsFromAPI => {
-            console.log("friends from API", friendsFromAPI)
-            let friendsUserObj = friendsFromAPI.map(friend => friend.user)
-            setFriendsObj(friendsUserObj)
-            setFriends(friendsFromAPI)
-        })
+            .then(friendsFromAPI => {
+                console.log("friends from API", friendsFromAPI)
+                let friendsUserObj = friendsFromAPI.map(friend => friend.user)
+                setFriendsObj(friendsUserObj)
+                setFriends(friendsFromAPI)
+            })
     }
 
     const getAllTheUsers = () => {
         getAllUsers()
-        .then(response => { 
-            console.log("allTheUser", response)
-            return setAllUsers(response)})
+            .then(response => {
+                return setAllUsers(response)
+            })
+    }
+
+    const notFriends = () => {
+        let notMyFriends = [...allUsers]
+
+        for (var i = 0, len = friendsObj.length; i < len; i++) {
+            for (var j = 0, len2 = notMyFriends.length; j < len2; j++) {
+                if (friendsObj[i].id === notMyFriends[j].id) {
+                    notMyFriends.splice(j, 1);
+                    len2 = notMyFriends.length;
+                }
+            }
+        }
+        return setAllUsersNotFriends(notMyFriends)
+    }
+
+    const searchResult = (searchString) => {
+        if (searchString.length > 0) {
+            let matchingUsers = allUsersNotFriends.filter(user => {
+                if (user.name.toLowerCase().includes(searchString) && user.id !== loggedInUser) {
+                    return true
+                }
+            })
+            setResult(matchingUsers)
+
+        }
+        else setResult([])
     }
 
     // handles deleting a friend by Id. RESETS STATE OF FRIENDS TO RENDER THE LIST AGAIN
     const handleDelete = (id) => {
         deleteFriend(id)
-        .then(() => getLoggedInUserFriends())
+            .then(() => getLoggedInUserFriends())
     }
 
     // handles adding a friend by Id. RESETS STATE OF FRIENDS TO RENDER THE LIST AGAIN
@@ -48,39 +75,12 @@ export const FriendsList = () => {
             userId: id
         }
         addFriend(newFriend)
-        .then(() => getLoggedInUserFriends())
+            .then(() => getLoggedInUserFriends())
     }
 
     const handleInputChange = (event) => {
         let selectedVal = event.target.value
         setSearch(selectedVal.toLowerCase())
-    }
-
-    const searchResult = (searchString) => {
-        if(searchString.length > 0) {
-                let matchingUsers = allUsersNotFriends.filter(user => {
-                    if(user.name.toLowerCase().includes(searchString) && user.id !== loggedInUser) {
-                        return true
-                    }
-                })
-                setResult(matchingUsers)
-            
-        }
-        else setResult([])
-    }
-
-    const notFriends = () => {
-        let notMyFriends = [...allUsers]
-
-        for (var i = 0, len = friendsObj.length; i < len; i++) { 
-            for (var j = 0, len2 = notMyFriends.length; j < len2; j++) { 
-                if (friendsObj[i].id === notMyFriends[j].id) {
-                    notMyFriends.splice(j, 1);
-                    len2=notMyFriends.length;
-                }
-            }
-        } 
-        return setAllUsersNotFriends(notMyFriends) 
     }
 
     useEffect(() => {
@@ -98,28 +98,28 @@ export const FriendsList = () => {
         <section className="friendList">
             <div className="searchBox">
                 <input type="text"
-                       id="search"
-                       className="friendSearch"
-                       required
-                       onChange={handleInputChange}
-                       placeholder="Search For a Friend" />
+                    id="search"
+                    className="friendSearch"
+                    required
+                    onChange={handleInputChange}
+                    placeholder="Search For a Friend" />
 
                 <div className="d-flex flex-column align-items-center">
                     {result.length === 0 ? <div></div> :
-                     result.map(result => 
-                        <SearchCard key={result.id}
-                                    result={result}
-                                    friends={friends}
-                                    handleAddFriend={handleAddFriend} />
-                     )}
+                        result.map(result =>
+                            <SearchCard key={result.id}
+                                result={result}
+                                friends={friends}
+                                handleAddFriend={handleAddFriend} />
+                        )}
                 </div>
             </div>
 
             <div className="d-flex justify-content-center">
-                {friends.map(friend => 
+                {friends.map(friend =>
                     <FriendCard key={friend.id}
-                                friend={friend}
-                                handleDelete={handleDelete} />
+                        friend={friend}
+                        handleDelete={handleDelete} />
                 )}
             </div>
         </section>
